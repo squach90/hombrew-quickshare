577c99e1d215864dd3b8a9ccab98a99c56549028
squach90 <160175686+squach90@users.noreply.github.com>
2025-03-29 13:56:31 +0100

    Switch to v1.1.0

--- Formula/quickshare.rb
+++ Formula/quickshare.rb
@@ -1,9 +1,9 @@
 class Quickshare < Formula
   desc "A CLI to quickly share files and link"
   homepage "https://github.com/squach90/homebrew-quickshare"
-  url "https://github.com/squach90/homebrew-quickshare/archive/refs/tags/v1.0.2.tar.gz"
-  sha256 "0bbe39b55a4cb3049cc66718db588b530e1bf0054c968313ac657f4530e65cfc"
-  version "1.0.2"
+  url "https://github.com/squach90/homebrew-quickshare/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "3888ac8424f0e7538d61bfc57356f415b2efdf04036eaf9e8f33859613d3a1e9"
+  version "1.1.0"
 
   depends_on "gum"
 
