6774db0973f28a6c05c734eb926d614e898affd4
squach90 <160175686+squach90@users.noreply.github.com>
2025-03-29 12:58:16 +0100

    Update hombrew

--- Formula/quickshare.rb
+++ Formula/quickshare.rb
@@ -1,9 +1,9 @@
 class QuickShare < Formula
   desc "A CLI to quickly share files and link"
   homepage "https://github.com/squach90/homebrew-quickshare"
-  url "https://github.com/squach90/homebrew-quickshare/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
-  version "1.0.0"
+  url "https://github.com/squach90/homebrew-quickshare/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "5eba714df46d2f11d13628a75af22c764860034910ac55d41049a3cb0c0ed39c"
+  version "1.0.1"
 
   depends_on "gum"
 
